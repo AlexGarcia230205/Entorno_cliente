ba69fb2c007dfea0015f3eefff2442ff57996745
Alex <alexgarciamartin2302@gmail.com>
2025-10-21 16:25:52 +0200

    Solución errores

--- calculo.js
+++ calculo.js
@@ -1,17 +1,15 @@
 //Declaramos las variables que vamos a usar
+const BOTON_CALCULAR = document.getElementById("calcular");
+const RESULTADO = document.getElementById("resultado");
+const NOTA_INTRODUCIDA = document.getElementById("nota");
+
+//Añadimos el evento al botón
+
+BOTON_CALCULAR.addEventListener("click", calcularNota);
 
 function calcularNota() {
-
-    /*Declaramos las constantes y variables, en este caso he puesto constantes ya que siempre serán
+  /*Declaramos las constantes y variables, en este caso he puesto constantes ya que siempre serán
     las etiquetas que tengan esos ids*/
-
-  const BOTON_CALCULAR = document.getElementById("calcular");
-  const RESULTADO = document.getElementById("resultado");
-  const NOTA_INTRODUCIDA = document.getElementById("nota");
-
-  //Añadimos el evento al botón
-
-  BOTON_CALCULAR.addEventListener("click", calcularNota);
 
   //Luego voy a eliminar los espacios en blanco y ha pasar el texto a número
 
@@ -22,36 +20,36 @@ function calcularNota() {
 
   //Si no es un número, mostrará un mensaje de error
 
-  if (isNaN(notaNumerica)){
-    RESULTADO.textContent = 'ERROR: Debes introducir un número válido.'
+  if (isNaN(notaNumerica)) {
+    RESULTADO.textContent = "ERROR: Debes introducir un número válido.";
     return;
   }
 
   //Aquí verificamos si el número que nos ha ofrecido es del 1 al 10, si no, mensaje de error.
 
-  if (notaNumerica < 0 || notaNumerica > 10){
-    RESULTADO.textContent = 'ERROR: Debes introducir un número entre el 0 y el 10.'
+  if (notaNumerica < 0 || notaNumerica > 10) {
+    RESULTADO.textContent =
+      "ERROR: Debes introducir un número entre el 0 y el 10.";
     return;
   }
 
   //Declaramos una variable que cambiará dependiendo de la nota que haya sacado.
 
-  let calificacionObtenida = '';
+  let calificacionObtenida = "";
 
-  if (notaNumerica < 5){
-    calificacionObtenida = 'Suspenso';
-  } else if (notaNumerica < 6){
-    calificacionObtenida = 'Aprobado';
-  } else if (notaNumerica < 7){
-    calificacionObtenida = 'Bien';
+  if (notaNumerica < 5) {
+    calificacionObtenida = "Suspenso";
+  } else if (notaNumerica < 6) {
+    calificacionObtenida = "Aprobado";
+  } else if (notaNumerica < 7) {
+    calificacionObtenida = "Bien";
   } else if (notaNumerica < 9) {
-    calificacionObtenida = 'Notable';
+    calificacionObtenida = "Notable";
   } else {
-    calificacionObtenida = 'Sobresaliente';
+    calificacionObtenida = "Sobresaliente";
   }
 
   //Mostramos un mensaje con la calificación obtenida
 
-  RESULTADO.textContent = `Has obtenido un ${calificacionObtenida}.`
-
+  RESULTADO.textContent = `Has obtenido un ${calificacionObtenida}.`;
 }
